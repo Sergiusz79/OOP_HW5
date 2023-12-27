529f068a33637b594d6c318fd9c25a3b48cb5147
Admin <mc.sergey30@gmail.com>
2023-12-27 21:44:28 +0100

    Final&Logging v01

--- src/main/java/org/example/View/ViewInterface.java
+++ src/main/java/org/example/View/ViewInterface.java
@@ -1,15 +1,10 @@
 package org.example.View;
 
-import java.util.List;
+
 
 public interface ViewInterface {
     int get();
 
     void set(String value);
 
-    void appendLog(String log);
-
-    public void appendLog(List list);
-
-    void showLog();
 }
